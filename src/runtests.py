f283df4d7dcaef5227f58cd962e6ce2c2824c603
klapen <oasaavedrac@gmail.com>
2018-06-18 10:22:53 -0500

    Fix bug to run all tests

--- src/runtests.py
+++ src/runtests.py
@@ -22,8 +22,10 @@ if __name__ == '__main__':
         elif opt in ("-t","--test-file"):
             print("*-*-*-*-*-* Testing file: "+arg+"*-*-*-*-*-*")
             execfile(arg)
-        else:
-            test_files = glob.glob("tests/test_*.py")
-            for test in test_files:
-                print("*-*-*-*-*-* Testing file: "+test+"*-*-*-*-*-*")
-                execfile(test)
+            sys.exit()
+
+    # If no option, run all tests
+    test_files = glob.glob("tests/test_*.py")
+    for test in test_files:
+        print("*-*-*-*-*-* Testing file: "+test+"*-*-*-*-*-*")
+        execfile(test)
